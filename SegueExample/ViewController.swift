533b14723c135a47f82c07ac85a9a7652f53d1de
Soumitra Bera <soumya9396@gmail.com>
2022-04-10 11:55:48 +0530

    Initial Commit

---
+++ SegueExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueExample
+//
+//  Created by Soumitra Bera on 10/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
